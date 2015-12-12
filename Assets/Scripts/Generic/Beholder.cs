8039ff0e86c5e859c066c95a8c56c8e6ada0a2bb
Awpteamoose <awpteamoose@gmail.com>
2015-12-12 15:50:57 +0200

    Initial

---
+++ Assets/Scripts/Generic/Beholder.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// A class that implements the Observer pattern without resorting to individual events, makes it all a lot more abstract.
+/// </summary>
+public static class Beholder
+{
+	/// <summary>
+	/// Example usage:<para />
+	/// Beholder.Trigger("event_name", ...);
+	/// </summary>
+	/// <param name="key">Plain string event name</param>
+	/// <param name="...">Up to 5 arguments to be passed to all subscribers</param>
+	public static void FlushCallbacks()
+	{
+		_subscriptions = new Dictionary<string, List<object>>();
+	}
+	public static void Trigger(string key)
+	{
+		_Trigger<object, object, object, object, object>(key, null, null, null, null, null);
+	}
+	public static void Trigger<T1>(string key, T1 arg1)
+	{
+		_Trigger<T1, object, object, object, object>(key, arg1, null, null, null, null);
+	}
+	public static void Trigger<T1, T2>(string key, T1 arg1, T2 arg2)
+	{
+		_Trigger<T1, T2, object, object, object>(key, arg1, arg2, null, null, null);
+	}
+	public static void Trigger<T1, T2, T3>(string key, T1 arg1, T2 arg2, T3 arg3)
+	{
+		_Trigger<T1, T2, T3, object, object>(key, arg1, arg2, arg3, null, null);
+	}
+	public static void Trigger<T1, T2, T3, T4>(string key, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+	{
+		_Trigger<T1, T2, T3, T4, object>(key, arg1, arg2, arg3, arg4, null);
+	}
+	public static void Trigger<T1, T2, T3, T4, T5>(string key, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
+	{
+		_Trigger<T1, T2, T3, T4, T5>(key, arg1, arg2, arg3, arg4, arg5);
+	}
+	private static void _Trigger<T1, T2, T3, T4, T5>(string key, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
+	{
+		if (!_subscriptions.ContainsKey(key)) return;
+		foreach (Callback<T1, T2, T3, T4, T5> cb in _subscriptions[key].ToList())
+		{
+			cb(arg1, arg2, arg3, arg4, arg5);
+		}
+	}
+
+	/// <summary>
+	/// Example usage:<para />
+	/// Beholder.Callback&lt;int, bool&gt; cb = (i, b) =&gt; { if (b) Debug.Log(i); };<para />
+	///	Beholder.Observe("event_name", cb);
+	/// </summary>
+	/// <param name="key">Plain string event name</param>
+	/// <param name="cb">Callback function which will be called</param>
+	/// <param name="sub">An optional subscription, that can be used to cancel any number of observations</param>
+	public static void Observe(string key, Callback cb, Subscription sub = null)
+	{
+		Callback<object, object, object, object, object> _cb = (arg1, arg2, arg3, arg4, arg5) => cb();
+		_Observe(key, _cb, sub);
+	}
+	public static void Observe<T1>(string key, Callback<T1> cb, Subscription sub = null)
+	{
+		Callback<T1, object, object, object, object> _cb = (arg1, arg2, arg3, arg4, arg5) => cb(arg1);
+		_Observe(key, _cb, sub);
+	}
+	public static void Observe<T1, T2>(string key, Callback<T1, T2> cb, Subscription sub = null)
+	{
+		Callback<T1, T2, object, object, object> _cb = (arg1, arg2, arg3, arg4, arg5) => cb(arg1, arg2);
+		_Observe(key, _cb, sub);
+	}
+	public static void Observe<T1, T2, T3>(string key, Callback<T1, T2, T3> cb, Subscription sub = null)
+	{
+		Callback<T1, T2, T3, object, object> _cb = (arg1, arg2, arg3, arg4, arg5) => cb(arg1, arg2, arg3);
+		_Observe(key, _cb, sub);
+	}
+	public static void Observe<T1, T2, T3, T4>(string key, Callback<T1, T2, T3, T4> cb, Subscription sub = null)
+	{
+		Callback<T1, T2, T3, T4, object> _cb = (arg1, arg2, arg3, arg4, arg5) => cb(arg1, arg2, arg3, arg4);
+		_Observe(key, _cb, sub);
+	}
+	public static void Observe<T1, T2, T3, T4, T5>(string key, Callback<T1, T2, T3, T4, T5> cb, Subscription sub = null)
+	{
+		Callback<T1, T2, T3, T4, T5> _cb = (arg1, arg2, arg3, arg4, arg5) => cb(arg1, arg2, arg3, arg4, arg5);
+		_Observe(key, _cb, sub);
+	}
+	private static void _Observe<T1, T2, T3, T4, T5>(string key, Callback<T1, T2, T3, T4, T5> cb, Subscription sub)
+	{
+		if (!_subscriptions.ContainsKey(key)) {
+			_subscriptions.Add(key, new List<object>());
+		}
+		_subscriptions[key].Add(cb);
+		if (sub != null) sub.items.Add(key, cb);
+	}
+	
+	/// <summary>
+	/// Example usage:<para />
+	/// Beholder.Subscription sub = new Beholder.Subscription();<para />
+	/// Beholder.Observe("event_name", cb, sub);<para />
+	/// Beholder.Cancel(sub);
+	/// </summary>
+	/// <param name="sub">The subscription to be cancelled</param>
+	public static void Cancel(Subscription sub)
+	{
+		foreach (var kv in sub.items)
+		{
+			_subscriptions[kv.Key].Remove(kv.Value);
+			if (_subscriptions[kv.Key].Count == 0) _subscriptions.Remove(kv.Key);
+		}
+	}
+
+	public delegate void Callback();
+	public delegate void Callback<in T1>(T1 arg1);
+	public delegate void Callback<in T1, in T2>(T1 arg1, T2 arg2);
+	public delegate void Callback<in T1, in T2, in T3>(T1 arg1, T2 arg2, T3 arg3);
+	public delegate void Callback<in T1, in T2, in T3, in T4>(T1 arg1, T2 arg2, T3 arg3, T4 arg4);
+	public delegate void Callback<in T1, in T2, in T3, in T4, in T5>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
+
+	public class Subscription
+	{
+		public Dictionary<string, object> items;
+		public Subscription()
+		{
+			items = new Dictionary<string, object>();
+		}
+	}
+
+	private static Dictionary<String, List<object>> _subscriptions = new Dictionary<string, List<object>>();
+}
---
+++ Assets/Scripts/Generic/Utils.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Object = UnityEngine.Object;
+
+public static class Utils
+{
+	public static IEnumerator After(float time, Action callback)
+	{
+	   yield return new WaitForSeconds(time);
+	   callback();
+	}
+
+	public static IEnumerator DoFor(float time, Action process, Action after = null)
+	{
+		while (time > 0)
+		{
+			yield return new WaitForEndOfFrame();
+			process();
+			time -= Time.deltaTime;
+		}
+		if (after != null) after();
+	}
+
+	public static Coroutine After(this MonoBehaviour caller, float time, Action callback)
+	{
+		return caller.StartCoroutine(After(time, callback));
+	}
+
+	public static Coroutine DoFor(this MonoBehaviour caller, float time, Action process, Action after = null)
+	{
+		return caller.StartCoroutine(DoFor(time, process, after));
+	}
+
+	public static void Cancel(this MonoBehaviour caller, Coroutine co)
+	{
+		caller.StopCoroutine(co);
+	}
+
+	public static GameObject DebugSphere(Vector3 position, float radius, Color color)
+	{
+		var s = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		s.transform.position = position;
+		s.transform.localScale = new Vector3(radius, radius, radius);
+		s.GetComponent<Renderer>().material.color = color;
+		Object.Destroy(s.GetComponent<Collider>());
+		return s;
+	}
+
+	public static void DrawSphere(this MonoBehaviour caller, Vector3 position, float time = 1, float radius = 1f, Color color = new Color())
+	{
+		if (color.r <= 0.1 && color.g <= 0.1 && color.b <= 0.1 && color.a <= 0.1) // new color
+			color = Color.green;
+
+		var s = DebugSphere(position, radius, color);
+		caller.After(time, () =>
+		{
+			Object.Destroy(s);
+		});
+	}
+
+	public static Vector3 calculateBestThrowSpeed(Vector3 origin, Vector3 target, float timeToTarget)
+	{
+		// calculate vectors
+		Vector3 toTarget = target - origin;
+		Vector3 toTargetXZ = toTarget;
+		toTargetXZ.y = 0;
+
+		// calculate xz and y
+		float y = toTarget.y;
+		float xz = toTargetXZ.magnitude;
+
+		// calculate starting speeds for xz and y. Physics forumulase deltaX = v0 * t + 1/2 * a * t * t
+		// where a is "-gravity" but only on the y plane, and a is 0 in xz plane.
+		// so xz = v0xz * t => v0xz = xz / t
+		// and y = v0y * t - 1/2 * gravity * t * t => v0y * t = y + 1/2 * gravity * t * t => v0y = y / t + 1/2 * gravity * t
+		float t = timeToTarget;
+		float v0y = y / t + 0.5f * Physics.gravity.magnitude * t;
+		float v0xz = xz / t;
+
+		// create result vector for calculated starting speeds
+		Vector3 result = toTargetXZ.normalized;        // get direction of xz but with magnitude 1
+		result *= v0xz;                                // set magnitude of xz to v0xz (starting speed in xz plane)
+		result.y = v0y;                                // set y to v0y (starting speed of y plane)
+
+		return result;
+	}
+}
